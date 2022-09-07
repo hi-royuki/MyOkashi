2ec950bb8cf535f97c2951e57f54c2cc39846168
hi-royuki <hiro.0621.ito3835@gmail.com>
2022-09-07 23:03:22 +0900

    [451]修正

--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -70,23 +70,22 @@ class OkashiData: ObservableObject{
             DispatchQueue.main.async {
                 //お菓子のリストを初期化
                 self.okashiList.removeAll()
-            }
-            //取得しているお菓子の数だけ処理
-            for item in items {
-                //お菓子の名称、掲載URL、画像URLをアンラップ
-                if let name = item.name,
-                   let link = item.url,
-                   let image = item.image {
-                    //一つのお菓子えお構造体でまとめて管理
-                    let okashi = OkashiItem(name: name, link: link, image: image)
-                    //@Publishedの変数を更新するときはメインスレッドで更新する必要がある
-                    DispatchQueue.main.async {
-                        //お菓子の配列へ追加
-                        self.okashiList.append(okashi)
+                
+                //取得しているお菓子の数だけ処理
+                for item in items {
+                    //お菓子の名称、掲載URL、画像URLをアンラップ
+                    if let name = item.name,
+                       let link = item.url,
+                       let image = item.image {
+                        //一つのお菓子えお構造体でまとめて管理
+                        let okashi = OkashiItem(name: name, link: link, image: image)
+                            //お菓子の配列へ追加
+                            self.okashiList.append(okashi)
                     }
                 }
                 print(self.okashiList)
             }
+            
             
             
             
